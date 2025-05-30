59d58ab712fed66228802e7802794f9a8a8c5c02
Happyfox001 <qianzhang.happyfox@gmail.com>
2025-05-30 23:10:25 +0800

    Delete RegexNode and related files

--- lib/nodeflow/RegexNode/RegexNodeModel.ts
+++
@@ -1,7 +0,0 @@
-import { RegexScriptSettings } from "@/lib/models/regex-script-model";
-
-export interface RegexNodeSettings extends RegexScriptSettings {
-  ownerId: string;
-  executeMode: "all" | "selected";
-  selectedScripts?: string[];
-}
